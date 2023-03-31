0c6d02620246ab88e3c896125c45610bc51a70b8
ShotMeow <shotmeow@gmail.com>
2023-03-31 16:56:01 +0300

    Added: Observed Custom

--- src/HUD/Layout/Layout.tsx
+++ src/HUD/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TeamBox from "./../Players/TeamBox";
 import MatchBar from "../MatchBar/MatchBar";
+import SeriesBox from "../MatchBar/SeriesBox";
 import Observed from "./../Players/Observed";
 import { CSGO, Team } from "csgogsi-socket";
 import { Match } from "../../api/interfaces";
@@ -102,7 +103,7 @@ export default class Layout extends React.Component<Props, State> {
           </div>
         </div>
         <Killfeed />
-        <Overview match={match} map={game.map} players={game.players || []} />
+        {/* <Overview match={match} map={game.map} players={game.players || []} /> */}
         <RadarMaps match={match} map={game.map} game={game} />
         <MatchBar
           map={game.map}
@@ -112,6 +113,7 @@ export default class Layout extends React.Component<Props, State> {
         />
         <Pause phase={game.phase_countdowns} />
         <Timeout map={game.map} phase={game.phase_countdowns} />
+        <SeriesBox map={game.map} phase={game.phase_countdowns} match={match} />
 
         <Tournament />
 
@@ -136,12 +138,12 @@ export default class Layout extends React.Component<Props, State> {
 
         <Trivia />
 
-        <MapSeries
+        {/* <MapSeries
           teams={[left, right]}
           match={match}
           isFreezetime={isFreezetime}
           map={game.map}
-        />
+        /> */}
         <div className={"boxes left"}>
           <UtilityLevel
             side={left.side}
